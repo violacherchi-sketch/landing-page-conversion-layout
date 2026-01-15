cfcbd4cc350e9ddbf0aa682aa073a01431608f12
violacherchi-sketch <viola.cherchi@vitek.digital>
2026-01-15 18:28:58 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ctaBtn = document.getElementById("cta-button");
+  const hiddenSection = document.getElementById("bottom-right");
 
+  ctaBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    hiddenSection.classList.add("visible");
+    ctaBtn.style.display = "none"; // Optional: Hide the CTA button
+  });
+});
